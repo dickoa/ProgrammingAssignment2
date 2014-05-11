8c103a404f3ea1cbccdfd53619e83e02b4ca9363
Dicko Ahmadou <dicko.ahmadou@gmail.com>
2014-05-11 13:56:29 +0000

    Create the two functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+##' .. content for \description{} (no empty lines) ..
+##'
+##' .. content for \details{} ..
+##' @title
+##' @param x
+##' @return
+##' @author
 makeCacheMatrix <- function(x = matrix()) {
+    inv <- NULL
 
+    set <- function(y) {
+        x <<- y
+        inv <- NULL
+    }
+
+    get <- function() x
+    setinv <- function(inverse) inv <<- inverse
+    getinv <- function() inv
+
+    list(set = set,
+         get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
 
-## Write a short comment describing this function
-
+##' .. content for \description{} (no empty lines) ..
+##'
+##' .. content for \details{} ..
+##' @title
+##' @param x
+##' @param ...
+##' @return
+##' @author
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinv()
+
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setinv(inv)
+    inv
 }
+
+## set.seed(1)
+## mymat <- makeCacheMatrix(matrix(rnorm(1e6), 1e3))
+## system.time(inv1 <- cacheSolve(mymat))
+## system.time(inv2 <- cacheSolve(mymat))
+## all.equal(inv1, inv2)
